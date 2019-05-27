2f8faeb34fe2a0c752b0d7ebaa933273125337dc
mbskoedt <36225641+mbskoedt@users.noreply.github.com>
2019-05-27 10:57:42 +0200

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,33 @@
+"use strict";
+
+// SINGLEPAGE FUNCTION
+
+// hide all pages
+function hideAllPages() {
+  let pages = document.querySelectorAll(".page");
+  for (let page of pages) {
+    page.style.display = "none";
+  }
+}
+
+// show page or tab
+function showPage(pageId) {
+  hideAllPages();
+  document.querySelector(`#${pageId}`).style.display = "block";
+
+}
+
+// set default page
+function setDefaultPage() {
+  let page = "home";
+  if (location.hash) {
+    page = location.hash.slice(1);
+  }
+  showPage(page);
+}
+
+setDefaultPage();
+
 // global variabel
 
 let madvarerArray = [];
@@ -6,6 +36,12 @@ let i = 0;
 let halvfjerdserMadkasseArray = [];
 let sundMadkasseArray = [];
 let usundMadkasseArray = [];
+
+// sammenlignings arrays
+
+let halvfjerdserMadkasseCompare = [];
+let sundMadkasseCompare = [];
+let usundMadkasseCompare = [];
 
 // fetch
 
@@ -16,6 +52,9 @@ fetch('json/madvarer.json')
   .then(function(json) {
     console.log(json);
     appendMadvarer(json.madvarer);
+    appendHalvfjerdserMadkasseCompare(json.halvfjerdserMadkasseCompare);
+    appendSundMadkasseCompare(json.sundMadkasseCompare);
+    appendUsundMadkasseCompare(json.usundMadkasseCompare);
   });
 
 // skubber fetchet data ind i nyt array
@@ -23,6 +62,25 @@ fetch('json/madvarer.json')
 function appendMadvarer(madvarer) {
   for (let madvare of madvarer) {
     madvarerArray.push(madvare);
+  };
+};
+
+function appendHalvfjerdserMadkasseCompare(halvfjerdserMadkasseCompare) {
+  for (let madvareHalvfjerdserMadkasse of halvfjerdserMadkasseCompare) {
+    halvfjerdserMadkasseCompare.push(madvareHalvfjerdserMadkasse);
+  };
+  console.log(halvfjerdserMadkasseCompare);
+};
+
+function appendSundMadkasseCompare(sundMadkasseCompare) {
+  for (let madvareSundMadkasseCompare of sundMadkasseCompare) {
+    sundMadkasseCompare.push(madvareSundMadkasseCompare);
+  };
+};
+
+function appendUsundMadkasseCompare(usundMadkasseCompare) {
+  for (let madvareUsundMadkasseCompare of usundMadkasseCompare) {
+    usundMadkasseCompare.push(madvareUsundMadkasseCompare);
   };
 };
 
@@ -62,7 +120,7 @@ function dropSund(ev) {
   ev.target.appendChild(document.getElementById(data));
   document.getElementById(data).classList.add("display-none");
   sundMadkasseArray.push(document.getElementById(data));
-  console.log(halvfjerdserMadkasseArray);
+  console.log(sundMadkasseArray);
   showMadvare();
 }
 
@@ -72,6 +130,41 @@ function dropUsund(ev) {
   ev.target.appendChild(document.getElementById(data));
   document.getElementById(data).classList.add("display-none");
   usundMadkasseArray.push(document.getElementById(data));
-  console.log(halvfjerdserMadkasseArray);
+  console.log(usundMadkasseArray);
   showMadvare();
 }
+
+// vis nyt array efter sortering
+
+function appendHalvfjerdserMadkasseArray(item) {
+  for (let item of halvfjerdserMadkasseArray) {
+    item.classList.remove("display-none");
+    console.log(item);
+    document.getElementById('slagterOleContainer').innerHTML +=
+      `<img src="${item.currentSrc}" width="300px"; z-index="10";>`;
+  };
+};
+
+/* giv resultat efter antal rigtige under vægt */
+
+let score = document.querySelector('#score');
+let aladin = 7;
+score.innerHTML = '<p> Dit resultat:</P>' + aladin + '<p>/10 Rigtige</p>';
+
+
+
+/* afgør hvilken feedback slagteren giver, på baggrund af score */
+
+
+function tale() {
+
+  if (aladin < 5) {
+    feedback.innerHTML = aladin + '<p> /10 Rigtige,<br><br> det kan du helt sikkert gøre bedre! <br><br> lad os støve historie bøgerne af, og prøve en gang til!</p>';
+  } else if (aladin <= 10) {
+    feedback.innerHTML = aladin + '<p> /10 Rigtige,<br><br> Det var flot, men der er plads til forbedringer <br><br> lad os støve historie bøgerne af, og prøve en gang til!</p>';
+  } else if (aladin > 10) {
+    feedback.innerHTML = aladin + '<p> /10 Rigtige,<br><br> Hold da op!<br><br> Der er vist en slagter gemt i dig! <br><br> Tryk på knappen for at prøve igen </p>';
+  }
+}
+
+tale();
